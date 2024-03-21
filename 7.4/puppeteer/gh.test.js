18baaaf49b1374f24b1fdbd1980ef5fc1b751e4b
Lazarov_O_M_QAMID_61 <oleg057orel@yandex.ru>
2024-03-21 12:10:29 +0300

    commit_task_74

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -10,17 +10,17 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
-  test("The h1 header content'", async () => {
+  test("The h1 header content", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
+    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub', {timeout: 70000});
   });
 
   test("The first link attribute", async () => {
     const actual = await page.$eval("a", link => link.getAttribute('href') );
-    expect(actual).toEqual("#start-of-content");
+    expect(actual).toEqual("#start-of-content", {timeout: 7000});
   });
 
   test("The page contains Sign in button", async () => {
@@ -29,6 +29,40 @@ describe("Github page tests", () => {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
+    expect(actual).toContain("Get started with Team", {timeout: 7000});
   });
 });
+
+  describe("Should check titles for GitHub pages", () => {
+    test("Should check Pricing page", async () => {
+      await page.goto("https://github.com/pricing");
+      const title = await page.title();
+      expect(title).toContain("Pricing · Plans for every developer · GitHub")
+    }, 6000);
+
+    test("Should check Enterprise page", async () => {
+      await page.goto("https://github.com/enterprise");
+      const title = await page.title();
+      expect(title).toContain("The AI Powered Developer Platform. · GitHub")
+    }, 6000);
+
+    test("Should check Features page", async () => {
+      await page.goto("https://github.com");
+      const featSel = "body > div.logged-out.env-production.page-responsive > footer > div.container-xl.p-responsive > div > div.col-6.col-sm-3.col-lg-2.mb-6.mb-md-2.pr-3.pr-lg-0.pl-lg-4 > ul > li:nth-child(1) > a";
+      await page.waitForSelector(featSel);
+      await page.click(featSel);
+      await page.waitForTimeout(7000);
+      const title = await page.title();
+      expect(title).toContain("Features | GitHub · GitHub");
+  }, 60000);
+
+    test("Should chek Features page title text", async() => {
+      await page.goto("https://github.com");
+      const featSel = "body > div.logged-out.env-production.page-responsive > footer > div.container-xl.p-responsive > div > div.col-6.col-sm-3.col-lg-2.mb-6.mb-md-2.pr-3.pr-lg-0.pl-lg-4 > ul > li:nth-child(1) > a";
+      await page.waitForSelector(featSel);
+      await page.click(featSel);
+      await page.waitForTimeout(7000);
+      const title1 = await page.$eval(`h1.h1-mktg`,link => link.textContent);
+      expect(title1).toEqual("The tools you need to build what you want.");
+    }, 70000);
+  })
